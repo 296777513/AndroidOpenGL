872313111cfee91943a4350b81204fd5311a7c73
liyachao <liyachao.knight@bytedance.com>
2019-04-17 14:38:57 +0800

    bugfix:update

--- app/src/main/java/com/example/liyachao/activity/MainActivity.kt
+++ app/src/main/java/com/example/liyachao/activity/MainActivity.kt
@@ -9,8 +9,6 @@ import android.view.View
 import com.example.liyachao.R
 import com.example.liyachao.permission.PermissionUtils
 import com.example.liyachao.utils.FileUtil
-import com.knight.alphavideoplayer.giftvideo.VideoController
-import com.knight.alphavideoplayer.utils.Constants
 import com.knight.glview.CameraMediaControl
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -22,7 +20,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : Activity(), View.OnClickListener {
 
     lateinit var control: CameraMediaControl
-    lateinit var videoController: VideoController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,8 +36,6 @@ class MainActivity : Activity(), View.OnClickListener {
              *   @isLoop 是否循环播放
              *   @mRoot父布局
              */
-            videoController = VideoController(mRoot, isLoop = false, playerType = VideoController.PLAYER_MEDIA, viewType = VideoController.VIEW_GLSURFACE)
-            videoController.prepareVideo(FileUtil.initPath() + "Alarms/boat8.mp4")
 //            videoController.start()
             FileUtil.initPath()
             mSwitchCamera.setOnClickListener(this)
@@ -57,28 +52,8 @@ class MainActivity : Activity(), View.OnClickListener {
 
             mSwitchCamera -> control.switchCamera()
 
-            mPlayMp4 -> {
-                Constants.isNewPlayer = false
-                initPlayer()
-                videoController.start()
-            }
 
-            mPlayNewMp4 -> {
-                Constants.isNewPlayer = true
-                initPlayer()
-                videoController.start()
-            }
-            mChangeMp4 -> index = (index + 1) % 4
         }
-    }
-
-    val newMp4 = arrayOf("boat1.mp4", "rocket5.mp4", "fastboat1.mp4", "airplane1.mp4")
-    val oldMp4 = arrayOf("boat.mp4", "rocket.mp4", "fastboat.mp4", "airplane.mp4")
-    var index = 0
-    private fun initPlayer() {
-        mRoot.removeView(videoController.alphaView.mView)
-        videoController = VideoController(mRoot, isLoop = false, playerType = VideoController.PLAYER_EXO, viewType = VideoController.VIEW_GLSURFACE)
-        videoController.prepareVideo(FileUtil.initPath() + "Alarms/${if (Constants.isNewPlayer) newMp4[index] else oldMp4[index]}")
     }
 
 
